6a6868c9b9bfa60836b6636d2041c78f0921484b
KrowK1ng <krow.lord@protonmail.com>
2021-09-05 15:38:18 +0300

    Copyright Update

--- src/app/commands/cmd_export_sprite_sheet.cpp
+++ src/app/commands/cmd_export_sprite_sheet.cpp
@@ -1,5 +1,7 @@
 // Aseprite
 // Copyright (C) 2001-2016  David Capello
+// LibreSprite
+// Copyright (C) 2021-2021  LibreSprite contributors
 //
 // This program is free software; you can redistribute it and/or modify
 // it under the terms of the GNU General Public License version 2 as
