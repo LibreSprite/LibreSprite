674327428bfbc11c8f7ddd0fbb0f7a868853b12c
Felipe Manga <lufeboma@gmail.com>
2021-09-21 18:13:27 +0100

    Allow scripts to be placed in subfolders/submenus (#220)

--- src/app/script/script_menu.cpp
+++ src/app/script/script_menu.cpp
@@ -6,20 +6,57 @@
 // published by the Free Software Foundation.
 
 #include "script_menu.h"
-#include <app/app.h>
-#include <app/app_menus.h>
-#include <app/commands/command.h>
-#include <app/commands/commands.h>
-#include <app/commands/params.h>
-#include <app/file_system.h>
-#include <app/ui/app_menuitem.h>
-#include <app/resource_finder.h>
-#include <base/bind.h>
-#include <base/path.h>
+#include "app/app.h"
+#include "app/app_menus.h"
+#include "app/commands/command.h"
+#include "app/commands/commands.h"
+#include "app/commands/params.h"
+#include "app/file_system.h"
+#include "app/ui/app_menuitem.h"
+#include "app/resource_finder.h"
+#include "base/bind.h"
+#include "base/path.h"
 #include "base/fs.h"
+#include "base/string.h"
+#include "script/script_object.h"
+#include "script/engine.h"
 
 namespace app {
 using namespace ui;
+
+  void scanFolder(const std::string& scriptsDir, Command* cmd_run_script, Menu* parent) {
+  FileSystemModule* fs = FileSystemModule::instance();
+  IFileItem* item = fs->getFileItemFromPath(scriptsDir);
+  if (item) {
+    Params params;
+    FileItemList list = item->children();
+    for (auto child : list) {
+      bool isFolder = child->isFolder();
+      std::string fullPath = base::fix_path_separators(child->fileName());
+      if (!isFolder) {
+        bool supported = false;
+        auto extension = base::string_to_lower(base::get_file_extension(fullPath));
+        for (auto& entry : script::Engine::getRegistry()) {
+          if (entry.second.hasFlag(extension)) {
+            supported = true;
+            break;
+          }
+        }
+        if (!supported)
+          continue;
+      }
+      auto cmd = isFolder ? nullptr : cmd_run_script;
+      params.set("filename", fullPath.c_str());
+      auto menuitem = new AppMenuItem(child->displayName().c_str(), cmd, params);
+      parent->addChild(menuitem);
+      if (isFolder) {
+        auto menu = new Menu();
+        scanFolder(fullPath, cmd_run_script, menu);
+        menuitem->setSubmenu(menu);
+      }
+    }
+  }
+}
 
 bool ScriptMenu::rebuildScriptsList(Menu* menu)
 {
@@ -43,23 +80,7 @@ bool ScriptMenu::rebuildScriptsList(Menu* menu)
   FileSystemModule* fs = FileSystemModule::instance();
   LockFS lock(fs);
   fs->refresh();
-  IFileItem* item = fs->getFileItemFromPath(scriptsDir);
-  if (item) {
-    Params params;
-    FileItemList list = item->children();
-    for (auto child : list) {
-      if (child->isFolder()) {
-        continue;
-      }
-      std::string fullPath = base::fix_path_separators(child->fileName());
-      params.set("filename", fullPath.c_str());
-      auto menuitem = new AppMenuItem(
-        child->displayName().c_str(),
-        cmd_run_script,
-        params);
-      menu->addChild(menuitem);
-    }
-  }
+  scanFolder(scriptsDir, cmd_run_script, menu);
   return true;
 }
 
