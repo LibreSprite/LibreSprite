b84f4fb016433810867b36a916a8917410aa0ac6
maujin111 <maurial593@gmail.com>
2021-10-06 05:47:32 -0500

    update

--- src/ui/listitem.h
+++ src/ui/listitem.h
@@ -24,7 +24,6 @@ namespace ui {
     void onPaint(PaintEvent& ev) override;
     void onResize(ResizeEvent& ev) override;
     void onSizeHint(SizeHintEvent& ev) override;
-    bool onProcessMessage(Message* msg) override;
 
   private:
     std::string m_value;
