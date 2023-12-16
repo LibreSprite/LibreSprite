4613ad0e4a8edc79dd0e44d922ea3ac9fa75e186
Felipe Manga <fmanga@macmini.Home>
2023-12-16 12:20:50 +0000

    Allow scripts to get sprite data as Base64-encoded PNG strings

--- src/app/script/api/image_script.cpp
+++ src/app/script/api/image_script.cpp
@@ -5,8 +5,11 @@
 // it under the terms of the GNU General Public License version 2 as
 // published by the Free Software Foundation.
 
+#include "base/base64.h"
 #include "script/script_object.h"
 #include "doc/image.h"
+#include "she/surface.h"
+#include "she/system.h"
 #include <cstring>
 
 class ImageScriptObject : public script::ScriptObject {
@@ -47,6 +50,10 @@ public:
     addMethod("getImageData", &ImageScriptObject::getImageData)
       .doc("creates an array containing all of the image's pixels.")
       .docReturns("All pixels in an array (Lua) or a Uint8Array (JS)");
+
+    addMethod("getPNGData", &ImageScriptObject::getPNGData)
+      .doc("Encodes the image as a PNG.")
+      .docReturns("The image as a Base64-encoded PNG string.");
   }
 
   void putImageData(script::Value::Buffer& data) {
@@ -63,6 +70,27 @@ public:
       std::size_t(m_image->getRowStrideSize()*m_image->height()),
       false
     };
+  }
+
+  std::string getPNGData() {
+    auto w = m_image->width();
+    auto h = m_image->height();
+    std::shared_ptr<she::Surface> surface{
+      she::instance()->createRgbaSurface(w, h),
+      [](auto s) {s->dispose();}
+    };
+    if (!surface)
+      return "";
+
+    for (auto y = 0; y < h; ++y) {
+      for (auto x = 0; x < w; ++x) {
+        surface->putPixel(m_image->getPixel(x, y), x, y);
+      }
+    }
+
+    std::string encoded;
+    base::encode_base64(she::instance()->encodeSurfaceAsPNG(surface.get()), encoded);
+    return "data:image/png;base64," + encoded;
   }
 
   void putPixel(int x, int y, int color) {
