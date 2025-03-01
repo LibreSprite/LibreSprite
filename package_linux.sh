a04829d83fc2005bba62919336d8b4268098b9e7
SoundDrill31 <84176052+sounddrill31@users.noreply.github.com>
2025-03-01 01:00:52 -0800

    Add AppImage Packing commands
    Use wildcard for file upload
    This allows us to build for multiple arch in the future if needed
    
    Feel free to revert this if it is not up to project standards
    
    Fix bash variable syntax for Linux workflow
    
    Fix Arch handling during upload
    
    Fix path to libresprite
    
    Fix data folder handling
    
    Copy over .so files instead of linking
    
    Handle data folder after the sharun stuff is done
    
    Fix path to data folder
    
    [skip ci]

--- package_linux.sh
+++ package_linux.sh
@@ -3,13 +3,17 @@
 
 out=$(pwd)
 src=$(pwd)
+APP="LibreSprite"
+ARCH="$(uname -m)"
 
 chmod +x libresprite
 
-mkdir -p LibreSprite
+mkdir -p LibreSprite/usr/bin
 
 mv ../../desktop/libresprite.desktop LibreSprite/
 cp ../../desktop/icons/hicolor/256x256/apps/libresprite.png LibreSprite/libresprite.png
+
+mv *.so* LibreSprite/usr/lib
 
 # Create AppImage with lib4bin and Sharun
 (
@@ -18,11 +22,21 @@ cd LibreSprite
 wget "https://raw.githubusercontent.com/VHSgunzo/sharun/refs/heads/main/lib4bin" -O ./lib4bin
 chmod +x ./lib4bin
 xvfb-run -a -- ./lib4bin -p -v -e -r -k -w \
-  /usr/bin/libresprite \
-  /usr/bin/data \
+  ../libresprite \
   /usr/lib/libpthread.so* \
   /usr/lib/librt.so* \
   /usr/lib/libstdc++.so* 
 ln ./sharun ./AppRun 
 ./sharun -g
 )
+
+# Maybe the data folder is being read during initial run
+# This lets the run complete with expected original locations and then
+# copies it over afterwards using the below command
+mv "$out"/data "$out"/LibreSprite/bin
+
+wget "https://github.com/AppImage/appimagetool/releases/download/continuous/appimagetool-$ARCH.AppImage" -O appimagetool
+chmod +x ./appimagetool
+./appimagetool --comp zstd \
+	--mksquashfs-opt -Xcompression-level --mksquashfs-opt 22 \
+	-n "$out"/LibreSprite "$out"/"$APP"-anylinux-"$ARCH".AppImage
