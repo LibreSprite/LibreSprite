323929fdc6e4c51ea234c2a0740656a0241c851d
SoundDrill31 <84176052+sounddrill31@users.noreply.github.com>
2025-03-01 00:52:36 -0800

    Prepare for Sharun+AppImage With this approach, you will not need to manually account for, and add all these dependencies by hand
    - This is also not hardcoding the architecture so you can easily run this as-is on aarch64 platforms
    
    Used Pixelpulse2 as reference
    https://github.com/sounddrill31/Pixelpulse2/blob/master/.github/workflows/main.yml

--- package_linux.sh
+++ package_linux.sh
@@ -1,61 +1,28 @@
 #!/bin/sh
 
-add()
-{
-  echo "adding $1"
-
-  if [ -e "/usr/lib/x86_64-linux-gnu/$1" ]; then
-      src="/usr/lib/x86_64-linux-gnu"
-  elif [ -e "/usr/lib/x86_64-linux-gnu/pulseaudio/$1" ]; then
-      src="/usr/lib/x86_64-linux-gnu/pulseaudio"
-  elif [ -e "/usr/lib64/$1" ]; then
-      src="/usr/lib64"
-  fi
-
-  if [ $src != $out ]; then
-      cp "$src/$1" "$out/$1"
-  fi
-
-  dependencies=$(readelf -d "$src/$1" | grep NEEDED | sed -En "s/[^\[]*\[([^]]*)\S*/\1/gp")
-
-  for dependency in $dependencies
-  do
-    if [ ! -f "$out/$dependency" ]; then
-        add $dependency
-    fi
-  done
-}
 
 out=$(pwd)
 src=$(pwd)
 
-add "libresprite"
-
-cp /usr/lib/libpthread.so* ./
-cp /usr/lib/librt.so* ./
-cp /usr/lib/libstdc++.so* ./
-
-rm libc.so*
-rm libm.so*
-
 chmod +x libresprite
 
-mkdir LibreSprite
-mkdir LibreSprite/usr
-mkdir LibreSprite/usr/bin
-mkdir LibreSprite/usr/lib
+mkdir -p LibreSprite
 
 mv ../../desktop/libresprite.desktop LibreSprite/
 cp ../../desktop/icons/hicolor/256x256/apps/libresprite.png LibreSprite/libresprite.png
 
-mv libresprite LibreSprite/usr/bin
-mv data LibreSprite/usr/bin
-mv *.so* LibreSprite/usr/lib
-
-wget https://github.com/AppImage/AppImageKit/releases/download/13/AppRun-x86_64 -O LibreSprite/AppRun
-chmod +x LibreSprite/AppRun
-
-wget https://github.com/AppImage/AppImageKit/releases/download/13/appimagetool-x86_64.AppImage -O appimagetool
-chmod +x appimagetool
-
-./appimagetool LibreSprite
+# Create AppImage with lib4bin and Sharun
+(
+export ARCH="$(uname -m)" # Just to be double sure
+cd LibreSprite
+wget "https://raw.githubusercontent.com/VHSgunzo/sharun/refs/heads/main/lib4bin" -O ./lib4bin
+chmod +x ./lib4bin
+xvfb-run -a -- ./lib4bin -p -v -e -r -k -w \
+  /usr/bin/libresprite \
+  /usr/bin/data \
+  /usr/lib/libpthread.so* \
+  /usr/lib/librt.so* \
+  /usr/lib/libstdc++.so* 
+ln ./sharun ./AppRun 
+./sharun -g
+)
