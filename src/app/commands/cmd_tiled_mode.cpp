8635231c905036637bcb8a23cf5799f3cb368ff4
Daniel Praźmo <d.prazmo@icloud.com>
2026-09-14 11:37:36 +0200

    Add keyboard shortcut to toggle Tiled Mode
    Bind Shift+T to the TiledMode command with axis=both, and make the
    command toggle back to no tiling when the requested mode is already
    active, so a single shortcut switches between no tiling and tiling in
    both axes.

--- src/app/commands/cmd_tiled_mode.cpp
+++ src/app/commands/cmd_tiled_mode.cpp
@@ -1,5 +1,5 @@
-// Aseprite
-// Copyright (C) 2001-2015  David Capello
+// Aseprite    | Copyright (C) 2001-2015 David Capello
+// LibreSprite | Copyright (C) 2026 LibreSprite contributors
 //
 // This program is free software; you can redistribute it and/or modify
 // it under the terms of the GNU General Public License version 2 as
@@ -65,7 +65,8 @@ bool TiledModeCommand::onChecked(Context* ctx)
 void TiledModeCommand::onExecute(Context* ctx)
 {
   const Document* doc = ctx->activeDocument();
-  Preferences::instance().document(doc).tiled.mode(m_mode);
+  auto& tiled = Preferences::instance().document(doc).tiled;
+  tiled.mode(tiled.mode() == m_mode ? filters::TiledMode::NONE : m_mode);
 }
 
 Command* CommandFactory::createTiledModeCommand()
